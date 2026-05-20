729203c588b28bec5ea4c08a1444cabe93817fd4
Ahmed Al-tell <140387178+Ahmedtell@users.noreply.github.com>
2026-05-20 14:39:55 +0300

    Remove ID Property from DTO and Command

--- Restaurants.Application/Restaurants/Commands/CreateRestaurant/CreateRestaurantCommand.cs
+++ Restaurants.Application/Restaurants/Commands/CreateRestaurant/CreateRestaurantCommand.cs
@@ -7,7 +7,6 @@ namespace Restaurants.Application.Restaurants.Commands.CreateRestaurant
     // This class is going to represenst the datd that is needed to create a new restaurant
     internal class CreateRestaurantCommand
     {
-        public int ID { get; set; }
         public string Name { get; set; } = default!;
         public string Description { get; set; } = default!;
         public string Category { get; set; } = default!;
--- Restaurants.Application/Restaurants/DTOs/CreateRestaurantDto.cs
+++ Restaurants.Application/Restaurants/DTOs/CreateRestaurantDto.cs
@@ -2,7 +2,6 @@
 {
     public class CreateRestaurantDto
     {
-        public int ID { get; set; }
         public string Name { get; set; } = default!;
         public string Description { get; set; } = default!;
         public string Category { get; set; } = default!;
